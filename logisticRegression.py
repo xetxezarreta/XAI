a3e3c373435827f8d6b307a4ba5b3ffbf11bd139
xetxezarreta <xabi1500@gmail.com>
2019-09-24 12:13:51 +0200

    Eli5 setup

--- logisticRegression.py
+++ logisticRegression.py
@@ -1,28 +1,15 @@
-import pandas
-from sklearn.model_selection import KFold
-from sklearn.model_selection import train_test_split
+from data.dataRead import get_train_test_data
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
 
-# leer datos
-filename = "data/pima-indians-diabetes.csv"
-names = ['preg','plas','pres','skin','test','mass','pedi','age','class']
-df = pandas.read_csv(filename, names=names)
-
-# preparar datos
-array = df.values
-X = array[:,0:8]
-Y = array[:,8]
-test_size = 0.33
-seed = 7
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size,random_state=seed)
+# obtenermos los datos para generar modelo
+x_train, x_test, y_train, y_test = get_train_test_data()
 
 # generar modelo
 model = LogisticRegression()
-model.fit(X_train, Y_train)
+model.fit(x_train, y_train)
 
 # ver resultados del modelo
-result = model.score(X_test, Y_test)
+result = model.score(x_test, y_test)
 
 print(result)
 
