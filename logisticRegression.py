860f0f8e113baebea745c77bab3de821027b77a0
xetxezarreta <xabi1500@gmail.com>
2019-09-24 10:56:03 +0200

    commit

--- logisticRegression.py
+++ logisticRegression.py
@@ -1,20 +1,28 @@
 import pandas
 from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
 
+# leer datos
 filename = "data/pima-indians-diabetes.csv"
 names = ['preg','plas','pres','skin','test','mass','pedi','age','class']
-
 df = pandas.read_csv(filename, names=names)
 
+# preparar datos
 array = df.values
 X = array[:,0:8]
 Y = array[:,8]
-kfold = KFold(n_splits=10, random_state=7)
+test_size = 0.33
+seed = 7
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size,random_state=seed)
 
+# generar modelo
 model = LogisticRegression()
-results = cross_val_score(model, X, Y, cv=kfold)
+model.fit(X_train, Y_train)
 
-print(results.mean())
+# ver resultados del modelo
+result = model.score(X_test, Y_test)
+
+print(result)
 
